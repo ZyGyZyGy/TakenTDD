7b08c7d9794a7254738abf559e1a838be4f9a638
Aleks <Aleks@LOK111-05>
2017-04-18 11:35:53 +0200

    1.3 Test fixtures

--- src/test/java/be/vdab/valueobjects/VeilingTest.java
+++ src/test/java/be/vdab/valueobjects/VeilingTest.java
@@ -1,27 +1,32 @@
 package be.vdab.valueobjects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VeilingTest {
 
+    private Veiling veiling;
+
+    @Before
+    public void before() {
+	veiling = new Veiling();
+    }
+
     @Test
     public void hoogsteBodVanNieuweVeilingIs0() {
-	Veiling veiling = new Veiling();
 	assertEquals(0, veiling.getHoogsteBod());
     }
-    
+
     @Test
     public void hoogsteBodNaEersteBodIs1000() {
-	Veiling veiling = new Veiling();
 	veiling.doeBod(1000);
 	assertEquals(1000, veiling.getHoogsteBod());
     }
-    
+
     @Test
     public void hoogsteBodNaMeerdereBoddenIs3000() {
-	Veiling veiling = new Veiling();
 	veiling.doeBod(1000);
 	veiling.doeBod(2000);
 	veiling.doeBod(3000);
