0ba2ffe4bc52953e4153ee1fc562a80955f6a266
Alex <jegorov.aleksandr@yahoo.com>
2017-04-19 13:23:09 +0200

    test

--- src/main/java/be/vdab/valueobjects/ISBN.java
+++ src/main/java/be/vdab/valueobjects/ISBN.java
@@ -9,45 +9,45 @@ public class ISBN {
     private static final long GROOTSTE_GETAL_MET_13_CIJFERS = 9999_999_999_999L;
     private static final Set<Short> MOGELIJKE_EERSTE_3_CIJFERS = new HashSet<>();
     private final long nummer;
-    
+
     static {
 	MOGELIJKE_EERSTE_3_CIJFERS.add((short) 978);
 	MOGELIJKE_EERSTE_3_CIJFERS.add((short) 979);
     }
-
-    public ISBN(long nummer) { 
-      if (nummer < KLEINSTE_GETAL_MET_13_CIJFERS 
-       || nummer > GROOTSTE_GETAL_MET_13_CIJFERS) { 
-        throw new IllegalArgumentException("Bevat geen 13 cijfers"); 
-      } 
-      short eerste3Cijfers = (short) (nummer / 10_000_000_000L); 
-        if (!MOGELIJKE_EERSTE_3_CIJFERS.contains(eerste3Cijfers)) { 
-        throw new IllegalArgumentException( 
-          "Begint niet met " + MOGELIJKE_EERSTE_3_CIJFERS); 
-        } 
-      long somEvenCijfers = 0; 
-      long somOnEvenCijfers = 0; 
-      long teVerwerkenCijfers = nummer / 10; 
-      for (int teller = 0; teller != 6; teller++) { 
-        somEvenCijfers += teVerwerkenCijfers % 10; 
-        teVerwerkenCijfers /= 10; 
-        somOnEvenCijfers += teVerwerkenCijfers % 10; 
-        teVerwerkenCijfers /= 10; 
-      } 
-      long controleGetal = somEvenCijfers * 3 + somOnEvenCijfers; 
-      long naastGelegenHoger10Tal = controleGetal - controleGetal % 10 + 10; 
-      long verschil = naastGelegenHoger10Tal - controleGetal; 
-      long laatsteCijfer = nummer % 10; 
-      if (verschil == 10) { 
-        if (laatsteCijfer != 0) { 
-          throw new IllegalArgumentException("Verkeerd controlegetal"); 
-        } 
-      } else { 
-        if (laatsteCijfer != verschil) { 
-          throw new IllegalArgumentException("Verkeerd controlegetal"); 
-        } 
-      } 
-      this.nummer = nummer; 
+    
+    // 9789027439642L
+    
+    public ISBN(long nummer) {
+	if (nummer < KLEINSTE_GETAL_MET_13_CIJFERS || nummer > GROOTSTE_GETAL_MET_13_CIJFERS) {
+	    throw new IllegalArgumentException("Bevat geen 13 cijfers");
+	}
+	short eerste3Cijfers = (short) (nummer / 10_000_000_000L);
+	if (!MOGELIJKE_EERSTE_3_CIJFERS.contains(eerste3Cijfers)) {
+	    throw new IllegalArgumentException("Begint niet met " + MOGELIJKE_EERSTE_3_CIJFERS);
+	}
+	long somEvenCijfers = 0;
+	long somOnEvenCijfers = 0;
+	long teVerwerkenCijfers = nummer / 10;
+	for (int teller = 0; teller != 6; teller++) {
+	    somEvenCijfers += teVerwerkenCijfers % 10;
+	    teVerwerkenCijfers /= 10;
+	    somOnEvenCijfers += teVerwerkenCijfers % 10;
+	    teVerwerkenCijfers /= 10;
+	}
+	long controleGetal = somEvenCijfers * 3 + somOnEvenCijfers;
+	long naastGelegenHoger10Tal = controleGetal - controleGetal % 10 + 10;
+	long verschil = naastGelegenHoger10Tal - controleGetal;
+	long laatsteCijfer = nummer % 10;
+	if (verschil == 10) {
+	    if (laatsteCijfer != 0) {
+		throw new IllegalArgumentException("Verkeerd controlegetal");
+	    }
+	} else {
+	    if (laatsteCijfer != verschil) {
+		throw new IllegalArgumentException("Verkeerd controlegetal");
+	    }
+	}
+	this.nummer = nummer;
     }
 
     @Override
@@ -55,4 +55,4 @@ public class ISBN {
 	return String.valueOf(nummer);
     }
 }
-//test
+// test
